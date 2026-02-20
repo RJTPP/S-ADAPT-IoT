f2c27a46ebbb1d5642de152f1dc799cfdeb1c378
RJTPP <97868966+RJTPP@users.noreply.github.com>
2026-02-20 17:55:47 +0700

    chore: enable pull-up on PB1

--- S-ADAPT/Core/Src/main.c
+++ S-ADAPT/Core/Src/main.c
@@ -606,7 +606,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : ENCODER_CLK_EXTI1_Pin */
   GPIO_InitStruct.Pin = ENCODER_CLK_EXTI1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(ENCODER_CLK_EXTI1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : ENCODER_PRESS_Pin */
